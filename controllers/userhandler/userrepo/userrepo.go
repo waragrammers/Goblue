e154d561661ce3eec3a036edc38e1e1752b7f2db
teddNe <eukigali@gmail.com>
2019-02-14 16:35:05 +0200

    Define User CRUD with clearn Archtec

---
+++ controllers/userhandler/userrepo/userrepo.go
@@ -0,0 +1,39 @@
+package userrepo
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/waragrammers/Goblue/controllers/userhandler"
+	"github.com/waragrammers/Goblue/models"
+)
+
+//UserRepo used to interact with BD
+type UserRepo struct {
+	DbConn *gorm.DB
+}
+
+//NewUserRepostory for initializing new userrepo
+func NewUserRepostory(DbConnection *gorm.DB) userhandler.UserRepostory {
+	return &UserRepo{DbConnection}
+}
+
+//CreateUser for creating user
+func (db *UserRepo) CreateUser() (string, error) {
+	message := "CreateUser function"
+	return message, nil
+}
+
+//GetAllUser for getting all users
+func (db *UserRepo) GetAllUser() ([]*models.User, error) {
+	return nil, nil
+}
+
+//GetuserByID get single user
+func (db *UserRepo) GetuserByID(id int) (*models.User, error) {
+	return nil, nil
+}
+
+//UpdateUser update user info
+func (db *UserRepo) UpdateUser(id int) (string, error) {
+	message := "Updated"
+	return message, nil
+}
---
+++ controllers/userhandler/userrepostory.go
@@ -0,0 +1,13 @@
+package userhandler
+
+import (
+	"github.com/waragrammers/Goblue/models"
+)
+
+//UserRepostory will be implemented by user handlers
+type UserRepostory interface {
+	CreateUser() (string, error)
+	GetAllUser() ([]*models.User, error)
+	GetuserByID(id int) (*models.User, error)
+	UpdateUser(id int) (string, error)
+}
